ec3c7fc39e8880ed89b8e01741705c5df587e174
ItsHope <1918429@students.wits.ac.za>
2022-04-23 17:56:05 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import "./App.css"
 import logo from './logo.svg';
+import design from './img/Design.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Website</h1>
+      <h1 classname ="websiten">
+        
+        React Website</h1>
       <section class ="header">
-      <li></li>
-      <li></li>
-      <li></li>
+       <li><a href="./componets/blogs/Blogpage">Blogs</a></li>
+       <li><a href="./componets/blogs/Blogpage">Blogs</a></li>
+       <li><a href="./componets/blogs/Blogpage">Blogs</a></li>
+               
+      </section>
+
+      <section>
+        <h2>Blogpost2</h2> 
+        <p>
+        After intensively engaging with especially “The Work of Art in the age of the internet” my choice of net/web art was Indirect Flights. Mr. Benjamin mentioned in his essay, “The work of Art in the age of its mechanical reproduction” that it is almost human nature to want to copy something so historically the idea of reproduction is not new. 
+
+The authenticity of an art work is what Benjamin describes as the aura, “a phenomena’s uniqueness” (Benjamin, year). His theory suggests that every original piece of art has an aura, “the aura of the sunset, the aura of nature”’ (Benjamin, 2015) that cannot be replaced. Mechanically or technologically reproduced art refuses to appreciate or acknowledge that aura. 
+
+He suggests that old forms of art have creativity and genius, value and mystery embedded in them and reproduction undoes that appreciation or status. 
+The work of Art in the age of the internet acknowledges the aura that Benjamin talks about but suggests that art on the internet still has evidence of “contemplative emersion” that usually is lost in mechanically reproduced art. This is because when viewing art on the internet we still somehow find ourselves staring. 
+My thoughts are, because the internet offers tools that allow art to be viewed in different new ways, this is why some digital art pieces can be viewed with music to assist with immersion like Indirect Flights.
+I was also able to use my cursor to move the art piece around the screen, I could do it quickly and slowly right after which I enjoyed and gave me “new art” seeing the artwork in motion. I thought it was mesmerizing. 
+ Another tool is the ability to be able to view art so closely that you can see the smallest details, “the display system has changed, you see the work better, close ups and etc.” (Carrier, 2020). Uncommon in net art because it is usually static (Danae, 2019), art on the internet today has taken this to a whole different level. 
+ There are zooming in and out tools that give the viewer the ability to engage with art like never before and completely indulge in the process.
+
+Danae mentioned that net art today can be seen as clunky, old fashioned or uncool. For a piece like Indirect Flight being able to interact with the artwork more can be seen as a future improvement. The piece is layered with photos, a zooming in and out tool would have been so ideal. With that said I still think the layers come together so nicely and I was still to see and appreciate its beauty. 
+
+        </p>
+        
+        <p>
+        References 
+1.	Benjamin. W (1935). The Work of Art in the Age of Mechanical Reproduction. Translated by J. A. Underwood, Penguin Books (2008). 
+
+2.	Carrier. D (2020). The Work of Art in the Age of the internet. 
+
+3.	Danae (2019). Net Art, Post-internet Art, New Aesthetics:
+The Fundamentals of Art on the Internet
+
+
+
+
+
+
+        </p>
+        <img src={design} alt="design"></img>
       </section>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -27,6 +65,8 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
 
